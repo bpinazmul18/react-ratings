ce782b0bed0cb576e5c90ebd300cd40c688656db
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2022-02-14 16:02:23 +0600

    Add full process to write rating reviews

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { FaStar } from "react-icons/fa";
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 export default function App() {
+  const [cValue, setCValue] = useState(0);
+  const [hValue, setHValue] = useState(undefined);
+  const stars = Array(5).fill(0);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>React ratings</h2>
+      <div className="rating-wrapper">
+        {stars.map((_, index) => {
+          return (
+            <FaStar
+              key={index}
+              size={24}
+              color={(hValue || cValue) > index ? "orange" : "black"}
+              onClick={() => setCValue(index + 1)}
+              onMouseOver={() => setHValue(index + 1)}
+              onMouseLeave={() => setHValue(undefined)}
+              style={{ cursor: "pointer" }}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
